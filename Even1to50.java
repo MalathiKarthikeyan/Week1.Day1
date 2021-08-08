c9cf620dca39a075a3b1f5f2670ded40ecee36e0
MalathiKarthikeyan <88610849+MalathiKarthikeyan@users.noreply.github.com>
2021-08-08 13:54:41 +0530

    Add files via upload

---
+++ Even1to50.java
@@ -0,0 +1,22 @@
+package week1class1;
+
+public class Even1to50 {
+
+	
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		for(int i=1;i<=50;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("Even="+i);
+			}
+			/*else
+			{
+				System.out.println("Odd="+i);
+			}
+		}*/
+			}
+
+			}}
